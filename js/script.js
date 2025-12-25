8195d469f6e5479cd5401370410732fcee57e8e2
CodeCrafters Hub <134312879+cchsolution@users.noreply.github.com>
2025-12-25 15:34:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(e) {
         $('#ytvid').tubular(options);
     }
     // Set the date we're counting down to
-    var countDownDate = new Date("Jan 31, 2025 01:37:25").getTime();
+    var countDownDate = new Date("Jan 31, 2026 01:37:25").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
